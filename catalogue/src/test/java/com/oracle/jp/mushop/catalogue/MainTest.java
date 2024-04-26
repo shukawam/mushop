28e7050b3cec5d1fb525f75ef6a3e4b69a093f1b
shukawam <shukawam@gmail.com>
2024-04-26 19:43:52 +0900

    Fixed test and actions config

--- catalogue/src/test/java/com/oracle/jp/mushop/catalogue/MainTest.java
+++ catalogue/src/test/java/com/oracle/jp/mushop/catalogue/MainTest.java
@@ -4,9 +4,6 @@ package com.oracle.jp.mushop.catalogue;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.Response;
-import org.eclipse.microprofile.metrics.Counter;
-import org.eclipse.microprofile.metrics.MetricRegistry;
-import jakarta.json.JsonArray;
 
 import io.helidon.microprofile.testing.junit5.HelidonTest;
 import io.helidon.metrics.api.MetricsFactory;
@@ -16,13 +13,9 @@ import org.junit.jupiter.api.AfterAll;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @HelidonTest
 class MainTest {
-
-    @Inject
-    private MetricRegistry registry;
 
     @Inject
     private WebTarget target;
@@ -33,30 +26,9 @@ class MainTest {
     }
 
     @Test
-    void testMicroprofileMetrics() {
-        String message = target.path("simple-greet/Joe").request().get(String.class);
-
-        assertThat(message, is("Hello Joe"));
-        Counter counter = registry.counter("personalizedGets");
-        double before = counter.getCount();
-
-        message = target.path("simple-greet/Eric").request().get(String.class);
-
-        assertThat(message, is("Hello Eric"));
-        double after = counter.getCount();
-        assertEquals(1d, after - before, "Difference in personalized greeting counter between successive calls");
-    }
-
-    @Test
     void testHealth() {
         Response response = target.path("health").request().get();
         assertThat(response.getStatus(), is(200));
-    }
-
-    @Test
-    void testPokemonTypes() {
-        JsonArray types = target.path("type").request().get(JsonArray.class);
-        assertThat(types.size(), is(18));
     }
 
 }
